ec046f7e0ec22af65292dcbc087a49ffe0374707
nwrnitaa <162811426+nwrnitaa@users.noreply.github.com>
2025-07-11 08:43:18 +0800

    Add files via upload

---
+++ profil.js
@@ -0,0 +1,8 @@
+const data = {
+  nama: "Rina",
+  email: "rina@email.com",
+  telepon: "08123456789",
+  alamat: "Jl. Kucing Bahagia No. 1"
+};
+
+localStorage.setItem("loginAktif", JSON.stringify(data));
